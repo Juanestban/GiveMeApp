fcddf00db7ebbd414330758bf708a512b413cf74
Juanestban <53704567+Juanestban@users.noreply.github.com>
2020-12-03 16:50:23 -0500

    install asyncStorage and creating api/endpoind

---
+++ app/api/endpoints.js
@@ -0,0 +1,10 @@
+const url = 'https://give-me-app.herokuapp.com/api'
+const users = '/users'
+
+export const signIn = `${users}/signin`
+export const signUp = `${users}/signup`
+export const dashboard = `${users}/me/dashboard`
+// enviroment
+export const signOut = `${users}/signout`
+
+export default url
---
+++ app/screens/Home/Status.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+const HomeStatus = () => {
+    return (
+        <View>
+            <Text>Soy el texto HomeStatus</Text>
+        </View>
+    )
+}
+
+export default HomeStatus
